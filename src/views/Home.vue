394831b1a6ef3779aceddd22f517b11dd4961d1e
Alex <alexandrapowley@gmail.com>
2019-04-17 06:57:49 -0600

    First commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,32 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="center" v-if="user === null">
+      <router-link to="/register" class="pure-button">Register</router-link> or
+      <router-link to="/login" class="pure-button">Login</router-link>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "home",
-  components: {
-    HelloWorld
+  components: {},
+  computed: {
+    user() {
+      return this.$store.state.user;
+    }
+  },
+  async created() {
+    await this.$store.dispatch("getUser");
   }
 };
 </script>
+
+<style>
+.empty-msg {
+  font-size: 3rem;
+}
+.center {
+  text-align: center;
+}
+</style>
